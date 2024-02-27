bc0ec6ea1e1fd345dbfad5ccbcfcb47a066743d5
tanujanair1109 <tanujanair11@gmail.com>
2024-02-27 12:18:01 +0530

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App';
+// import AuthProvider from './context/AuthContext';
+// import { QueryProvider } from './lib/react-query/QueryProvider';
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <BrowserRouter>
+//         <QueryProvider>
+//             <AuthProvider>
+//                 <App/>
+//             </AuthProvider>
+//         </QueryProvider>
+//     </BrowserRouter>
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider  from "@/context/AuthContext";
+import { QueryProvider } from "@/lib/react-query/QueryProvider";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
